893c933fbf83aa5c84d23ce21cf93ec1297a7e6e
Alex Johnson <alex.johnson@ramseysolutions.com>
2025-05-23 15:03:33 -0500

    feat: add the crypto thing

--- main.go
+++ main.go
@@ -2,23 +2,44 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
-	"time"
+	"net/url"
+	"os"
 
 	"github.com/joho/godotenv"
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file") 
-	} 
+		log.Fatal("Error loading .env file")
+	}
 
-	// apiKey := os.Getenv("CMC_KEY")
+	apiKey := os.Getenv("CMC_KEY")
 
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest", nil)
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
 
-	fmt.Println("Hello World")
+	q := url.Values{}
+	q.Add("start", "1")
+	q.Add("limit", "5000")
+	q.Add("convert", "USD")
+
+	req.Header.Set("Accepts", "application/json")
+	req.Header.Add("X-CMC_PRO_API_KEY", apiKey)
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request to server")
+		os.Exit(1)
+	}
+	respBody, _ := io.ReadAll(resp.Body)
+	fmt.Println(string(respBody))
 }
-
